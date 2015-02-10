ea541231128ca7fdb0f3f1836aad37240b218415
matthelosh <matthelosh@gmail.com>
2015-02-10 07:03:49 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="id">
+ <head>
+  <meta charset="UTF-8" />
+  <title>Sistem Peminjaman Alat Praktek</title>
+ 
+ </head>
+ <body>
+  <div id="wrapper">
+    <header></header>
+    <section id="main-content">
+     <article>
+      <h1>Assalamualaikum</h1>
+      <p>Bismillahirrohmaan</p>
+     </article>
+  </div>
+ </body>
+</html>
